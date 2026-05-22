18dbc92117f7cb86a14385a4148bb676678dea62
GamilaHegazy <gamilakhaledhegazy@gmail.com>
2026-05-22 20:48:40 +0300

    fetching all users (backend addition)

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { RoleGuard } from '../auth/role.guard';
@@ -12,6 +14,21 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @UseGuards(RoleGuard)
+  async findAll(@Query('teamId') teamId?: string) {
+    if (teamId) {
+      return this.userService.findByTeam(teamId);
+    }
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  @UseGuards(RoleGuard)
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
 
   @Patch(':userId/team')
   @UseGuards(RoleGuard)
